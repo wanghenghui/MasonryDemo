c2d5953f939e6483645e2553d60142cabab0ba41
YY <11@YY.local>
2016-03-10 14:19:28 +0800

    Revert "学习适配 Masonry"
    This reverts commit 91e11fa6de6c1f56a5a8c5de74d0e88f47c54e27.

---
+++ MasonryDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MasonryDemo
+//
+//  Created by YY on 16/3/10.
+//  Copyright © 2016年 WHH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MasonryDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MasonryDemo
+//
+//  Created by YY on 16/3/10.
+//  Copyright © 2016年 WHH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
